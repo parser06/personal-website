3fd39fb1c00edccaff9b82dd912b9bd47bee27a9
Paras Goel <goel.paras@gmail.com>
2026-07-23 00:04:22 -0700

    Temp for Git Pages Display

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 
-  site: "https://parasgoel.com",
+  site: "https://parser06.github.io",
 
   vite: {
     plugins: [tailwindcss()]
